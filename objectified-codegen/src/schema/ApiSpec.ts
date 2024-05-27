0a95e062cef40654c109e87e41f7ce9285e0ae10
Ken Suenobu <ksuenobu@fastmail.com>
2024-05-26 21:04:04 -0600

    #4: addresses servers section parsing

--- objectified-codegen/src/schema/ApiSpec.ts
+++ objectified-codegen/src/schema/ApiSpec.ts
@@ -1,13 +1,16 @@
 import {Components} from '../schema/Components';
 import {Path} from './Path';
+import {ServerStore} from '../stores/ServerStore';
 
 export class ApiSpec {
   private components: Components;
   private paths: Path[];
+  private servers: ServerStore[];
 
   constructor(private readonly segment: any) {
     this.components = new Components(segment);
     this.paths = [];
+    this.servers = [];
 
     const pathList = Object.keys(segment['paths']);
 
@@ -23,11 +26,21 @@ export class ApiSpec {
         this.paths.push(new Path(pathUrl, pathVerb, pathWithVerb));
       }
     }
+
+    for(const server of segment['servers'] ?? []) {
+      this.servers.push(new ServerStore(server));
+    }
+
+    throw new Error('Top level tags not yet parsed');
+    throw new Error('Top level security not yet parsed');
+    throw new Error('Top level info not yet parsed');
   }
 
   public setComponents = (components: Components) => this.components = components;
   public setPaths = (paths: Path[]) => this.paths = paths;
+  public setServer = (servers: ServerStore[]) => this.servers = servers;
 
   public getComponents = (): Components => this.components;
   public getPaths = (): Path[] => this.paths;
+  public getServers = (): ServerStore[] => this.servers;
 }
