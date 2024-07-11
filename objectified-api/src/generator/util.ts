9ad71ddc8a6a660b393b645f37edee7b04d4f04a
Ken Suenobu <ksuenobu@fastmail.com>
2024-07-11 11:56:15 -0600

    moved utility functions into util, added documentation

---
+++ objectified-api/src/generator/util.ts
@@ -0,0 +1,68 @@
+/**
+ * This is a type formatter that converts types from one type to another, matching object types for
+ * GraphQL and for JSON Schema definitions.
+ *
+ * @param key The key to parse for.
+ * @param value The value of the key.
+ * @returns Altered value if applicable, otherwise, returns the original value that was sent.
+ */
+export function typeFormatter(key: string, value: string) {
+  if (key.toLowerCase() === 'type') {
+    switch(value.toLowerCase()) {
+      case 'number':
+      case 'integer':
+      case 'float':
+      case 'double':
+        return 'Number';
+
+      case 'string':
+        return 'String';
+
+      case 'boolean':
+        return 'Boolean';
+
+      case 'object':
+        return 'Object';
+
+      case 'array':
+        return 'Array';
+
+      default:
+        return value;
+    }
+  }
+
+  return value;
+}
+
+/**
+ * Takes in a JSON Schema snippet, and returns a TypeScript type that matches the definition.
+ * Translates $ref values, parsing off the `#/component/schemas` field, and attaching a TypeScript well formatted
+ * type in its place.  Converts enumeration values to TypeScript enumeration values.  Converts numeric types to
+ * `number`, string types to `string`, and arrays to `type[]` with recursion.
+ *
+ * @param properties The JSON schema snippet.
+ * @returns string containing the translated value.
+ */
+export function propertyToType(properties: any): string {
+  const type = properties['type'];
+  const ref = properties['$ref'];
+
+  if (ref) {
+    return ref.substring(ref.lastIndexOf('/') + 1) + 'Dto';
+  }
+
+  if (properties['enum']) {
+    return '[ ' + properties['enum'].map((x) => `'${x}'`).join(' | ') + ' ]';
+  }
+
+  switch(type.toLowerCase()) {
+    case 'integer':
+      return 'number';
+
+    case 'array':
+      return `${propertyToType(properties['items'])}[]`;
+  }
+
+  return type;
+}
