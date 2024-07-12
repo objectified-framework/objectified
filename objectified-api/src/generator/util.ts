7f63ef2574b603555728ead3e05cbd7b9e5e399f
Ken Suenobu <ksuenobu@fastmail.com>
2024-07-11 19:31:57 -0600

    starting ref code for controllers

--- objectified-api/src/generator/util.ts
+++ objectified-api/src/generator/util.ts
@@ -79,3 +79,13 @@ export function propertyToType(properties: any): string {
 export function initCap(str: string): string {
   return str.charAt(0).toUpperCase() + str.substring(1);
 }
+
+export function toCamelCase(str: string): string {
+  const s =
+    str &&
+    str.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+      .map(x => x.slice(0, 1).toUpperCase() + x.slice(1).toLowerCase())
+      .join('');
+
+  return s.slice(0, 1).toLowerCase() + s.slice(1);
+}
