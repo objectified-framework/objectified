91f8d9064ba3678f3430c1848451b9cc81661a8d
Kenji Suenobu <ksuenobu@fastmail.com>
2024-09-01 22:21:51 -0600

    added sidebar, starting to adjust layout

---
+++ objectified-ui/src/components/Sidebar/sidebar.tsx
@@ -0,0 +1,35 @@
+import styles from './sidebar.module.css';
+import {Box} from "@mui/material";
+
+export default function Sidebar() {
+  return (
+    <>
+      <div className={styles.sidebar}>
+        <Box sx={{
+          width: '100%', height: '48px', backgroundColor: '#ff0', border: '2px solid #000',
+          borderRadius: '6px', padding: '12px'
+        }}>
+          Tag
+        </Box>
+
+        <div height={'20px'}>&nbsp;</div>
+
+        <Box sx={{
+          width: '100%', height: '48px', backgroundColor: '#fff', border: '2px solid #000',
+          borderRadius: '6px', padding: '12px'
+        }}>
+          Path
+        </Box>
+
+        <div height={'20px'}>&nbsp;</div>
+
+        <Box sx={{
+          width: '100%', height: '48px', backgroundColor: '#fff', border: '2px solid #000',
+          borderRadius: '6px', padding: '12px'
+        }}>
+          Schema
+        </Box>
+      </div>
+    </>
+  );
+}
