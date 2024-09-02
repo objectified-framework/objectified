e6c8bbb0b6f751e859634045492ac182d2089817
Kenji Suenobu <ksuenobu@fastmail.com>
2024-09-02 11:46:44 -0600

    added drag and drop functionality to new tags

--- objectified-ui/src/components/Flow.tsx
+++ objectified-ui/src/components/Flow.tsx
@@ -9,33 +9,72 @@ import {
   addEdge,
   useNodesState,
   useEdgesState,
-  type OnConnect,
+  type OnConnect, useReactFlow,
 } from "@xyflow/react";
 
 import "@xyflow/react/dist/style.css";
 
-import { initialNodes, nodeTypes, type CustomNodeType } from "./nodes";
-import { initialEdges, edgeTypes, type CustomEdgeType } from "./edges";
+// import { initialNodes, nodeTypes, type CustomNodeType } from "./nodes";
+// import { initialEdges, edgeTypes, type CustomEdgeType } from "./edges";
+import { initialNodes } from "./nodes";
+import { initialEdges } from "./edges";
+import {useDnD} from "./drag-and-drop/DnDContext";
+
+let id = 0;
+const getId = () => `dndnode_${id}`;
 
 export default function App() {
-  const [nodes, , onNodesChange] = useNodesState<CustomNodeType>(initialNodes);
-  const [edges, setEdges, onEdgesChange] =
-    useEdgesState<CustomEdgeType>(initialEdges);
+  const [nodes, setNodes, onNodesChange] = useNodesState<CustomNodeType>(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState<CustomEdgeType>(initialEdges);
+  const { screenToFlowPosition } = useReactFlow();
+  const [type] = useDnD();
   const onConnect: OnConnect = useCallback(
     (connection) => setEdges((edges) => addEdge(connection, edges)),
     [setEdges]
   );
 
+  const onDragOver = useCallback((event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+    console.log('[Flow] On Drag Over');
+  }, []);
+
+  const onDrop = useCallback((event) => {
+    event.preventDefault();
+
+    if (!type) {
+      return;
+    }
+
+    const position = screenToFlowPosition({
+      x: event.clientX,
+      y: event.clientY,
+    });
+
+    const newNode = {
+      id: getId(),
+      type,
+      position,
+      data: { label: `${type} node` },
+    }
+
+    setNodes((nds) => nds.concat(newNode));
+
+    console.log('Position', position);
+  }, [screenToFlowPosition, type]);
+
   return (
     <>
       <ReactFlow<CustomNodeType, CustomEdgeType>
         nodes={nodes}
-        nodeTypes={nodeTypes}
+        // nodeTypes={nodeTypes}
         onNodesChange={onNodesChange}
         edges={edges}
-        edgeTypes={edgeTypes}
+        // edgeTypes={edgeTypes}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        onDrop={onDrop}
+        onDragOver={onDragOver}
         fitView
         sx={{ width: '300'}}
       >
--- objectified-ui/src/components/Sidebar/sidebar.tsx
+++ objectified-ui/src/components/Sidebar/sidebar.tsx
@@ -1,14 +1,22 @@
 import styles from './sidebar.module.css';
 import {Box} from "@mui/material";
+import {useDnD} from "../drag-and-drop/DnDContext";
 
 export default function Sidebar() {
+  const [_, setType] = useDnD();
+
+  const onDragStart = (event, nodeType) => {
+    setType(nodeType);
+    event.dataTransfer.effectAllowed = 'move';
+  }
+
   return (
     <>
       <div className={styles.sidebar}>
         <Box sx={{
           width: '100%', height: '48px', backgroundColor: '#ff0', border: '2px solid #000',
-          borderRadius: '6px', padding: '12px'
-        }}>
+          borderRadius: '6px', padding: '12px', cursor: '-webkit-grab'
+        }} draggable onDragStart={ (e) => onDragStart(e, 'tag')}>
           Tag
         </Box>
 
@@ -16,8 +24,8 @@ export default function Sidebar() {
 
         <Box sx={{
           width: '100%', height: '48px', backgroundColor: '#0ff', border: '2px solid #000',
-          borderRadius: '6px', padding: '12px'
-        }}>
+          borderRadius: '6px', padding: '12px', cursor: '-webkit-grab'
+        }} draggable onDragStart={ (e) => onDragStart(e, 'path') }>
           Path
         </Box>
 
@@ -25,8 +33,8 @@ export default function Sidebar() {
 
         <Box sx={{
           width: '100%', height: '48px', backgroundColor: '#f0f', border: '2px solid #000',
-          borderRadius: '6px', padding: '12px'
-        }}>
+          borderRadius: '6px', padding: '12px', cursor: '-webkit-grab'
+        }} draggable onDragStart={ (e) => onDragStart(e, 'schema') }>
           Schema
         </Box>
       </div>
