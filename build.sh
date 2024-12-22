bc30f584049761e789a9f44a5d7a16ba729054b5
Ken Suenobu <ksuenobu@fastmail.com>
2024-12-21 19:21:16 -0700

    fix: adds load of database

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@
 # Project level builder
 
 echo "Building Objectified Master Project"
+
+# Load the database first
+cd objectified-db && sh ./build.sh && cd .. && echo "DB Loaded." && echo
+
+# Load the OpenAPI toolset next
 cd objectified-api && sh ./build.sh && cd .. && echo "API project built properly."
 
 
