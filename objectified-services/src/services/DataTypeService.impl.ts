e1136bca406e94bd6245931b770d663b1c4a7566
Ken Suenobu <ksuenobu@fastmail.com>
2024-12-24 13:56:02 -0700

    fix: updated to include list and get

--- objectified-services/src/services/DataTypeService.impl.ts
+++ objectified-services/src/services/DataTypeService.impl.ts
@@ -1,26 +1,55 @@
 import { Logger } from '@nestjs/common';
-import {DataTypeService, ServiceResponse} from "../generated/services";
+import {DataTypeService, ResponseNotFound, ResponseOk, ServiceResponse} from "../generated/services";
 import {DataTypeDto} from "../generated/dto";
+import {DataTypeDao} from "../generated/dao";
 
 export class DataTypeServiceImpl implements DataTypeService {
   private readonly logger = new Logger(DataTypeServiceImpl.name);
 
-  createDataType(request, dataTypeDto: DataTypeDto): Promise<ServiceResponse<DataTypeDto>> {
+  async createDataType(request, dataTypeDto: DataTypeDto): Promise<ServiceResponse<DataTypeDto>> {
     this.logger.log(`[createDataType] dataTypeDto=${JSON.stringify(dataTypeDto, null, 2)}`);
     return Promise.resolve(undefined);
   }
 
-  getDataTypeById(request, id: string): Promise<ServiceResponse<DataTypeDto>> {
+  async getDataTypeById(request, id: string): Promise<ServiceResponse<DataTypeDto>> {
     this.logger.log(`[getDataTypeById] id=${id}`);
-    return Promise.resolve(undefined);
+
+    const dataTypeDao = new DataTypeDao();
+    const result = await dataTypeDao.getById(id)
+      .then((x) => {
+        this.logger.log('[getDataTypeById] Object retrieved', x);
+        return x;
+      })
+      .catch((x) => {
+        this.logger.error('[getDataTypeById] Object failed to retrieve', x);
+        return null;
+      });
+
+    if (!result) {
+      return ResponseNotFound(id);
+    }
+
+    return ResponseOk(JSON.stringify(result));
   }
 
-  listDataTypes(request): Promise<ServiceResponse<DataTypeDto[]>> {
+  async listDataTypes(request): Promise<ServiceResponse<DataTypeDto[]>> {
     this.logger.log('[listDataTypes]');
-    return Promise.resolve(undefined);
+
+    const dataTypeDao = new DataTypeDao();
+    const results = (await dataTypeDao.getAll()) ?? [];
+
+    if (results) {
+      for (const mappedResult of results) {
+        mappedResult.ownerId = null;
+      }
+    }
+
+    this.logger.log(`[listDataTypes] Return list`, results);
+
+    return ResponseOk(JSON.stringify(results));
   }
 
-  updateDataType(request, id: string, dataTypeDto: DataTypeDto): Promise<ServiceResponse<DataTypeDto>> {
+  async updateDataType(request, id: string, dataTypeDto: DataTypeDto): Promise<ServiceResponse<DataTypeDto>> {
     this.logger.log(`[updateDataType] id=${id} dataTypeDto=${JSON.stringify(dataTypeDto, null, 2)}`);
     return Promise.resolve(undefined);
   }
