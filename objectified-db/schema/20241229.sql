ed6945ef1bfccecd2a1a8ca47ef5f442a7fa3636
Ken Suenobu <ksuenobu@fastmail.com>
2024-12-29 18:07:49 -0700

    fix: updates database, adds fields

--- objectified-db/schema/20241229.sql
+++ objectified-db/schema/20241229.sql
@@ -31,3 +31,23 @@ INSERT INTO obj.tenant_user (tenant_id, user_id, enabled) VALUES
     ((SELECT id FROM obj.tenant WHERE name='Objectified Project'),
      (SELECT id FROM obj.user WHERE email_address='ksuenobu@fastmail.com'),
      true);
+
+--- Fields
+
+DROP TABLE IF EXISTS obj.field CASCADE;
+DROP INDEX IF EXISTS idx_field_unique_name;
+
+CREATE TABLE obj.field (
+    id UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    tenant_id UUID NOT NULL REFERENCES obj.tenant(id),
+    data_type_id UUID NOT NULL REFERENCES obj.data_type(id),
+    name VARCHAR(80) NOT NULL,
+    description VARCHAR(4096) NOT NULL,
+    default_value TEXT,
+    enabled BOOLEAN NOT NULL DEFAULT true,
+    create_date TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
+    update_date TIMESTAMP WITHOUT TIME ZONE,
+    delete_date TIMESTAMP WITHOUT TIME ZONE
+);
+
+CREATE UNIQUE INDEX idx_field_unique_name ON obj.field(tenant_id, UPPER(name));
