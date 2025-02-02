3975dcec1ca67f474a1a68e6f2f2a79c57495ed8
Ken Suenobu <ksuenobu@fastmail.com>
2025-02-02 15:13:15 -0700

    fix: corrected data load for drop column statements

--- objectified-db/scripts/20250202-193003.sql
+++ objectified-db/scripts/20250202-193003.sql
@@ -1,2 +1,2 @@
-ALTER TABLE obj.class_property DROP COLUMN id;
-ALTER TABLE obj.object_property DROP COLUMN id;
+ALTER TABLE obj.class_property DROP COLUMN IF EXISTS id;
+ALTER TABLE obj.object_property DROP COLUMN IF EXISTS id;
