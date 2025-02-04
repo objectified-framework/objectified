169a9f0360a58587cc3d71e1b2f145252210e318
Ken Suenobu <ksuenobu@fastmail.com>
2025-02-03 21:05:18 -0700

    #65: fix - adds instance current table

---
+++ objectified-db/scripts/20250204-040501.sql
@@ -0,0 +1,11 @@
+-- Latest instance data store
+
+DROP TABLE IF EXISTS obj.instance_current CASCADE;
+DROP INDEX IF EXISTS idx_instance_current_unique;
+
+CREATE TABLE obj.instance_current (
+    instance_id UUID NOT NULL REFERENCES obj.instance(id),
+    instance_data JSONB
+);
+
+CREATE UNIQUE INDEX idx_instance_current_unique ON obj.instance_current(instance_id);
