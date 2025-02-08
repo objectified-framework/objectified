4b80d7823e2acad50d2931fcdf76e9e5460bccf7
Ken Suenobu <ksuenobu@fastmail.com>
2025-02-08 15:25:06 -0700

    #74: enforces tenancy when inserting into class_property

--- objectified-db/scripts/20250208-222444.sql
+++ objectified-db/scripts/20250208-222444.sql
@@ -1,16 +1,14 @@
--- Review
 CREATE OR REPLACE FUNCTION enforce_class_property_tenant()
 RETURNS TRIGGER AS $$
 BEGIN
     IF (SELECT tenant_id FROM obj.class WHERE id = NEW.class_id) !=
        (SELECT tenant_id FROM obj.property WHERE id = NEW.property_id) THEN
         RAISE EXCEPTION 'Class and Property must belong to the same tenant';
-END IF;
-RETURN NEW;
+    END IF;
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER trg_class_property_tenant
+CREATE OR REPLACE TRIGGER trigger_class_property_tenant
     BEFORE INSERT OR UPDATE ON obj.class_property
     FOR EACH ROW EXECUTE FUNCTION enforce_class_property_tenant();
-
