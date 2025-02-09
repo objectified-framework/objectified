919a0ba5c636a3510bf27cf5404f36846afc269d
Ken Suenobu <ksuenobu@fastmail.com>
2025-02-08 20:29:16 -0700

    #81: adds tenancy check on instance creation

--- objectified-db/scripts/20250209-032907.sql
+++ objectified-db/scripts/20250209-032907.sql
@@ -1,4 +1,7 @@
--- Enforce tenancy on instance insertion
+-- This stored procedure ensures that the tenancy ID and user ID specified for a class ID of an instance
+-- has proper access in place to create an instance.  The tenancy of the user must be valid, and must be
+-- a user of the tenancy listed.  Otherwise, the instance will not be allowed to be created.
+
 CREATE OR REPLACE FUNCTION enforce_instance_tenancy()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -15,8 +18,8 @@ RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS trg_enforce_instance_tenancy ON obj.instance;
-CREATE TRIGGER trg_enforce_instance_tenancy
+DROP TRIGGER IF EXISTS trigger_enforce_instance_tenancy ON obj.instance;
+CREATE TRIGGER trigger_enforce_instance_tenancy
     BEFORE INSERT OR UPDATE ON obj.instance
     FOR EACH ROW EXECUTE FUNCTION enforce_instance_tenancy();
 
