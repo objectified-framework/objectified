e0d25f231cc0c3d0a2d3ee130fc63d2d2f61a20e
Ken Suenobu <ksuenobu@fastmail.com>
2025-02-15 15:15:07 -0700

    #92: chore - moves the schema display into its own component

--- objectified-ui/src/app/classes/page.tsx
+++ objectified-ui/src/app/classes/page.tsx
@@ -1,16 +1,7 @@
 'use client';
 
 import {
-  Button,
-  CircularProgress,
   Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogContentText,
-  DialogActions,
-  Stack,
-  ToggleButtonGroup,
-  ToggleButton,
 } from "@mui/material";
 import {SearchOutlined} from '@mui/icons-material';
 import {useState, useEffect} from "react";
@@ -20,15 +11,13 @@ import {formItems, tableItems} from "@/app/classes/index";
 import AutoForm from "@/app/components/common/AutoForm";
 import {useSession} from 'next-auth/react';
 import {errorDialog} from "@/app/components/common/ConfirmDialog";
-import * as yaml from 'yaml';
+import {SchemaDialog} from "@/app/components/class-properties/SchemaDialog";
 
 const Classes = () => {
   const { data: session } = useSession();
   const [open, setOpen] = useState<boolean>(false);
   const [schemaOpen, setSchemaOpen] = useState<boolean>(false);
-  const [schema, setSchema] = useState<string>('');
-  const [schemaLoading, setSchemaLoading] = useState<boolean>(true);
-  const [schemaFormat, setSchemaFormat] = useState<string>('json');
+  const [selectedClassId, setSelectedClassId] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [dataPayload, setDataPayload] = useState([]);
   const [selectedLine, setSelectedLine] = useState({});
@@ -113,16 +102,8 @@ const Classes = () => {
 
   const showSchemaClicked = async (payload: any) => {
     console.log(payload);
+    setSelectedClassId(payload.id);
     setSchemaOpen(true);
-    await getClassSchema(payload.id)
-      .then((x) => {
-        setSchema(JSON.stringify(x.results, null, 2));
-        setSchemaLoading(false);
-      })
-      .catch((x) => {
-        setSchema(`{ "result": "Schema failed to retrieve: ${x}" }`);
-        setSchemaLoading(false);
-      });
   }
 
   const handleSchemaViewChange = (event, val: string) => {
@@ -135,72 +116,12 @@ const Classes = () => {
 
   const closeSchemaClicked = () => {
     setSchemaOpen(false);
-    setSchemaFormat('json');
-    setSchema('');
-    setSchemaLoading(true);
+    setSelectedClassId('');
   }
 
   return (
     <>
-      <Dialog
-        open={schemaOpen}
-        scroll={'paper'}
-        aria-labelledby="scroll-dialog-title"
-        aria-describedby="scroll-dialog-description"
-        maxWidth={'md'}
-        fullWidth
-        PaperProps={{ style: {
-            minHeight: '90%',
-            maxHeight: '90%',
-          }}}
-      >
-        <DialogTitle id="scroll-dialog-title">
-          <Stack direction={'row'}>
-            <div style={{ width: '60%' }}>
-              Schema
-            </div>
-            <div style={{ width: '40%', textAlign: 'right' }}>
-              <ToggleButtonGroup
-                color="primary"
-                value={schemaFormat}
-                exclusive
-                onChange={handleSchemaViewChange}
-                aria-label="Format"
-              >
-                <ToggleButton value="json">JSON</ToggleButton>
-                <ToggleButton value="yaml">YAML</ToggleButton>
-              </ToggleButtonGroup>
-            </div>
-          </Stack>
-        </DialogTitle>
-        <DialogContent dividers={true}>
-          <DialogContentText
-            id="scroll-dialog-description"
-            tabIndex={-1}
-          >
-            {schemaLoading && (
-              <>
-                <CircularProgress/>
-              </>
-            )}
-
-            {schema && schemaFormat === 'json' && (
-              <pre>
-                {JSON.stringify(JSON.parse(schema), null, 2)}
-              </pre>
-            )}
-
-            {schema && schemaFormat === 'yaml' && (
-              <pre>
-                {yaml.stringify(JSON.parse(schema))}
-              </pre>
-            )}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={closeSchemaClicked} variant={'contained'}>Close</Button>
-        </DialogActions>
-      </Dialog>
+      <SchemaDialog schemaOpen={schemaOpen} classId={selectedClassId} closeSchemaClicked={() => closeSchemaClicked()}/>
 
       <Dialog fullWidth open={open} onClose={handleClose}
               maxWidth={'md'}
---
+++ objectified-ui/src/app/components/class-properties/SchemaDialog.tsx
@@ -0,0 +1,129 @@
+'use client';
+
+import {
+  Button,
+  CircularProgress,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Stack,
+  ToggleButtonGroup,
+  ToggleButton,
+} from "@mui/material";
+import {SearchOutlined} from '@mui/icons-material';
+import {useState, useEffect} from "react";
+import DataListTable from "@/app/components/common/DataListTable";
+import {listClasses, putClass, saveClass, deleteClass, getClassSchema} from "@/app/services/class";
+import {formItems, tableItems} from "@/app/classes/index";
+import AutoForm from "@/app/components/common/AutoForm";
+import {useSession} from 'next-auth/react';
+import {errorDialog} from "@/app/components/common/ConfirmDialog";
+import * as yaml from 'yaml';
+
+export interface ISchemaDialog {
+  schemaOpen: boolean;
+  classId: string;
+  closeSchemaClicked: () => void;
+}
+
+export const SchemaDialog = (props: ISchemaDialog) => {
+  const [schemaFormat, setSchemaFormat] = useState<string>('json');
+  const [schemaLoading, setSchemaLoading] = useState<boolean>(true);
+  const [schema, setSchema] = useState<string>('');
+
+  const handleSchemaViewChange = (event, val: string) => {
+    if (val === null) {
+      return;
+    }
+
+    setSchemaFormat(val);
+  }
+
+  const closeClicked = () => {
+    setSchemaFormat('json');
+    setSchema('');
+    setSchemaLoading(true);
+    props.closeSchemaClicked();
+  }
+
+  useEffect(() => {
+    if (props.classId) {
+      setSchemaLoading(true);
+      getClassSchema(props.classId)
+        .then((x: any) => {
+          setSchema(JSON.stringify(x.results, null, 2));
+          setSchemaLoading(false);
+        })
+        .catch((x) => {
+          setSchema(`{ "result": "Schema failed to retrieve: ${x}" }`);
+          setSchemaLoading(false);
+        });
+    }
+  }, [props.classId]);
+
+  return (
+    <Dialog
+      open={props.schemaOpen}
+      scroll={'paper'}
+      aria-labelledby="scroll-dialog-title"
+      aria-describedby="scroll-dialog-description"
+      maxWidth={'md'}
+      fullWidth
+      PaperProps={{ style: {
+          minHeight: '90%',
+          maxHeight: '90%',
+        }}}
+    >
+      <DialogTitle id="scroll-dialog-title">
+        <Stack direction={'row'}>
+          <div style={{ width: '60%' }}>
+            Schema
+          </div>
+          <div style={{ width: '40%', textAlign: 'right' }}>
+            <ToggleButtonGroup
+              color="primary"
+              value={schemaFormat}
+              exclusive
+              onChange={handleSchemaViewChange}
+              aria-label="Format"
+            >
+              <ToggleButton value="json">JSON</ToggleButton>
+              <ToggleButton value="yaml">YAML</ToggleButton>
+            </ToggleButtonGroup>
+          </div>
+        </Stack>
+      </DialogTitle>
+      <DialogContent dividers={true}>
+        <DialogContentText
+          id="scroll-dialog-description"
+          tabIndex={-1}
+        >
+          {schemaLoading && (
+            <>
+              <CircularProgress/>
+            </>
+          )}
+
+          {schema && schemaFormat === 'json' && (
+            <pre>
+                {JSON.stringify(JSON.parse(schema), null, 2)}
+              </pre>
+          )}
+
+          {schema && schemaFormat === 'yaml' && (
+            <pre>
+                {yaml.stringify(JSON.parse(schema))}
+              </pre>
+          )}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => {
+          closeClicked();
+        }} variant={'contained'}>Close</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
