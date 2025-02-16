73499a4925b3bfcdaea6113c6250c29e4d130aba
Ken Suenobu <ksuenobu@fastmail.com>
2025-02-16 16:42:35 -0700

    fix: adds version number in all pages

--- objectified-ui/src/app/login/page.tsx
+++ objectified-ui/src/app/login/page.tsx
@@ -1,8 +1,12 @@
 'use client';
 
+import {
+  Link,
+} from "@mui/material";
 import LoginForm from '@/app/components/login/LoginForm';
 import {useSession} from "next-auth/react";
 import {useRouter} from "next/navigation";
+import {VERSION} from "@/app/page";
 
 const Login = () => {
   const { data: session } = useSession();
@@ -12,9 +16,29 @@ const Login = () => {
 
   if (!session) {
     return (
-      <div style={{ margin: '0', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-        <LoginForm/>
-      </div>
+      <>
+        <div style={{ margin: '0',
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)'
+        }}>
+          <LoginForm/>
+        </div>
+        <div style={{ position: 'absolute',
+          top: '0',
+          left: '0',
+          width: '100%',
+          height: '32px',
+          borderBottom: '1px solid #000',
+          padding: '4px',
+        }}>
+          <Link href={'https://www.objectified.dev/'}>About</Link>
+        </div>
+        <div style={{ position: 'absolute', bottom: 0, right: 0, color: '#000', paddingRight: '5px' }}>
+          v{VERSION}
+        </div>
+      </>
     );
   } else {
     router.push('/');
--- objectified-ui/src/app/page.tsx
+++ objectified-ui/src/app/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import Dashboard from "@/app/components/Dashboard";
 
+export const VERSION="0.0.1"
+
 export default function Home() {
   return (
     <main>
       <Dashboard/>
+      <div style={{ position: 'absolute', bottom: 0, right: 0, color: '#000', paddingRight: '5px' }}>
+        v{VERSION}
+      </div>
     </main>
   );
 }
