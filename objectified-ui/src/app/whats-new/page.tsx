83e812f1728eafffaffac10f1631c8e4a75f5a48
Kenji Suenobu <ksuenobu@fastmail.com>
2025-03-11 12:21:32 -0600

    fix: updated whats new, added more logos

--- objectified-ui/src/app/whats-new/page.tsx
+++ objectified-ui/src/app/whats-new/page.tsx
@@ -31,6 +31,7 @@ const WhatsNew = () => {
         <li> Added username and password authentication</li>
         <li> More fixes to the layout in the base screens</li>
         <li> Changed login screen to look more professional</li>
+        <li> Added logos</li>
       </div>
     </div>
   );
